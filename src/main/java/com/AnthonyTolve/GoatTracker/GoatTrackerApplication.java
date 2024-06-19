d6ae0fc54d9cc74d63d523d4cfbded1e2275ba52
anthonytolve <tolve43@gmail.com>
2024-06-19 09:26:17 -0400

    first commit

---
+++ src/main/java/com/AnthonyTolve/GoatTracker/GoatTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.AnthonyTolve.GoatTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoatTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoatTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AnthonyTolve/GoatTracker/GoatTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AnthonyTolve.GoatTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoatTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
